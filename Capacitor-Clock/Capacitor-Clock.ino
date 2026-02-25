ee851ce322a0251af8fff042ebd4b4d725b89ed5
Red-Singularity <49411538+Red-Singularity@users.noreply.github.com>
2026-02-25 00:29:31 -0500

    adjusted code to display to lcd
    began filling out code and was able to make code that displayed to a 2x16 display that can display the general GUI

---
+++ Capacitor-Clock/Capacitor-Clock.ino
@@ -0,0 +1,57 @@
+// Capacitor Clock main code
+// Started 1/24/2026
+// communicates  with a screen, an encoder, some buttons, a stepper motor, and some power circuitry
+// to explode a small capacitor when the set time has been reached
+
+#include <LiquidCrystal_I2C.h> // include I2C library
+#include <AccelStepper.h> // using accel stepper library for motor positioning
+
+#define MotorInterfaceType 4 // defines stepper as a 4 wire
+
+// Creates an instance of stepper class
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+AccelStepper myStepper(MotorInterfaceType, 1, 3, 2, 4);
+
+LiquidCrystal_I2C lcd(0x27,20,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+const int stepsPerRevolution = 2048;
+
+void setup() {
+  // serial setup for debugging
+  Serial.begin(115200);
+
+  //stepper setup
+  myStepper.setMaxSpeed(1000.0);
+  myStepper.setAcceleration(50.0);
+  myStepper.setSpeed(200);
+  myStepper.moveTo(2048);
+
+  // LCD setup
+  lcd.init(); // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(2,0);
+  lcd.print("Cap Clock V1");
+  lcd.setCursor(0,1);
+  lcd.print("By Mal & Dustin");
+  delay(2000);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  displayFormat(); // display the main format tot he display
+  displayDate(11, 6, 2000);
+
+
+
+  /*
+    if (myStepper.distanceToGo() == 0)
+    myStepper.moveTo(-myStepper.currentPosition());
+
+  // Move the motor one step
+  myStepper.run();
+  */
+
+}
---
+++ Capacitor-Clock/LCD.ino
@@ -0,0 +1,30 @@
+
+// main code pertaining to the LCD display. shows date and time
+
+void displayFormat(){
+  lcd.setCursor(0,0);
+  lcd.print("Time:   :  :  ");
+  lcd.setCursor(0,1);
+  lcd.print("Date:   /  /    ");
+}
+
+void displayTime(int hours, int minutes, int days){
+  lcd.setCursor(7,0);
+  lcd.print(hours);
+  lcd.setCursor(10,0);
+  lcd.print(minutes);
+  lcd.setCursor(13,0);
+}
+
+void displayDate(int month, int day, int year){
+  String month_s = String(month); // cast integers to strings
+  String day_s = String(day);
+  String year_s = String(year);
+
+  lcd.setCursor(6,1);
+  lcd.print(month_s);
+  lcd.setCursor(9,1);
+  lcd.print(day_s);
+  lcd.setCursor(12,1);
+  lcd.print(year_s);
+}
