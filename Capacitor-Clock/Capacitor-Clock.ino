da2cf96f528e2d0194eaba30250192f3991551db
Red-Singularity <49411538+Red-Singularity@users.noreply.github.com>
2026-03-21 04:53:49 -0400

    working time adjust
    got time adjustment with encoder to work

--- Capacitor-Clock/Capacitor-Clock.ino
+++ Capacitor-Clock/Capacitor-Clock.ino
@@ -7,6 +7,7 @@
 #include <AccelStepper.h> // using accel stepper library for motor positioning
 #include <DS3231.h> // include library for RTC module
 #include <Wire.h>
+#include <RotaryEncoder.h> // include library for rotary encoder
 
 #define MotorInterfaceType 4 // defines stepper as a 4 wire
 #define I_sense 36 // current sense pin
@@ -29,13 +30,21 @@ AccelStepper myStepper(MotorInterfaceType, Step1, Step3, Step2, Step4);
 LiquidCrystal_I2C lcd(0x27,20,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 DS3231 myRTC; // create instance of the real time clock
+
+RotaryEncoder rotary_encoder(DT, CLK, RotaryEncoder::LatchMode::TWO03); // Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
+
+enum encoder{clockwise, counter_clockwise, nothing};  // clock wise is interpreted as 0, counter clockwise as 1, nothing as 3
+
 bool century = false;
 bool h12Flag = true;
 bool pmFlag = false;
 byte alarmDay, alarmHour, alarmMinute, alarmSecond, alarmBits;
 bool alarmDy, alarmH12Flag, alarmPmFlag;
+int menu_location = 0; // total different adjustable fields for clock display
 
 const int stepsPerRevolution = 2048;
+unsigned long settings_time = 0;
+unsigned long settings_time_old = 0;
 
 void setup() {
   // serial setup for debugging
@@ -52,7 +61,7 @@ void setup() {
   pinMode(Button_2, INPUT);
   pinMode(LED, OUTPUT);
   pinMode(Fan, OUTPUT);
-  pinMode(SW, INPUT);
+  pinMode(SW, INPUT_PULLUP);
   pinMode(DT, INPUT);
   pinMode(CLK, INPUT);
 
@@ -87,8 +96,8 @@ void setup() {
 }
 
 void loop() {
-  static int settings = 1; // create settings flag
-  int settings_Time = 0;
+  encoder rotation; // create variable rotation as part of encoder enumerated type 
+  static int settings; // create settings flag
 
   bool CenturyBit; // this bit is required as an input to the month function and cannot be input as a true or false to the function directly
   int Year = 2000 + myRTC.getYear();
@@ -97,27 +106,93 @@ void loop() {
   int Hour = myRTC.getHour(h12Flag, pmFlag);
   int Minute = myRTC.getMinute();
   int Second = myRTC.getSecond();
+  int Encoder_button = digitalRead(SW);
 
   displayDate(Month, Day, Year);
   displayTime(Hour, Minute, Second);
 
-  if(SW == 0  || settings == 1){ // Encoder button was pressed. Enter setting adjustment mode
-    if(settings == 0){
+  if(Encoder_button == 0){
+      settings_time = millis(); // if entering function due to button reset settings time
+      settings_time_old = millis();
       settings = 1;
     }
-    if(settings == 1 && settings_Time > 2000){ // if settings time is greater than 2 seconds leave settings mode
+
+  if(settings == 1){ // Encoder button was pressed. Enter setting adjustment mode
+    settings_time = settings_time - settings_time_old; // read how long its been since an input
+    Serial.print("  settings time; ");
+    Serial.print(settings_time);
+    if(settings_time > 5000){ // cut menu adjust out of loop if its been too long since input
       settings = 0;
-    }
-    lcd.setCursor(19, 0);
-    if((int(millis()/500))%2==0){ //blink cursor every half second
-      lcd.print("|");
+      // also clear cursor here later
     }
     else{
-      lcd.print(" ");
+      settings = 1;
     }
+    menu_adjust();
+    settings_time = millis();
+  }
 
-    // due to the display being strange the 4th row is an extension of the 2nd starting at 20
+  Serial.println(); // print new line
+}
+
+
+
+void menu_adjust(){
+  encoder rotation = nothing;
+  int Encoder_button = digitalRead(SW);
+  static bool select = 0; // flag for entering selection of specific menus
+  if(select == 0){
+    rotation = Encoder(); // returns if encoder rotated clockwise or counterclockwise
+  }
+
+  if(settings_time > 200 && Encoder_button == 0){
+    select = 1;
+  }
+  else if(settings_time > 4900){
+    select = 0; // allow a different menu item to be selected when leaving menu settings screen
+  }
+
+  if(rotation == clockwise){
+    menu_location = menu_location+1;
+    if(menu_location > 10){menu_location = 0;} // total of 11 menu locations
+  }
+
+  else if(rotation == counter_clockwise){
+    menu_location = menu_location-1;
+    if(menu_location < 0){menu_location = 10;}
+  }
+
+  Serial.print("  menu location=");
+  Serial.print(menu_location);
+  Serial.print("  select=");
+  Serial.print(select);
+
+  // adjust time in hours on the main clock
+  if(menu_location == 0 && select == 1){
+    int hour = encoder_count(24); // use encoder to select number between 1 and 12
+    myRTC.setHour(hour); // adjust hour register in RTC
+    int minute = myRTC.getMinute();
+    int second = myRTC.getSecond();
+    displayTime(hour, minute, second); // call display time function again to update display
   }
 
 
+  // adjust time in minutes on main clock
+  if(menu_location == 1 && select == 1){
+    int minute = encoder_count(60); // use encoder to select number between 1 and 60
+    myRTC.setMinute(minute); // adjust hour register in RTC
+    int hour = myRTC.getHour(h12Flag, pmFlag);
+    int second = myRTC.getSecond();
+    displayTime(hour, minute, second); // call display time function again to update display
+  }
+
+
+  if(((int(millis()/500))%2==0) && (settings_time < 4900)){ //blink cursor every half second
+    lcd.print("|");
+  }
+  else{
+    lcd.print(" ");
+  }
+
+  // due to the display being strange the 4th row is an extension of the 2nd starting at 20
 }
--- Capacitor-Clock/Encoder.ino
+++ Capacitor-Clock/Encoder.ino
@@ -1,20 +1,56 @@
-void Encoder(){
-  static int counter = 0; 
-  static int aState;
-  static int aLastState;  
+encoder Encoder(){ // returns enumerated type encoder that is value clockwise or counter clockwise
+  int newPos;
+  encoder rotation; // create variable rotation as part of encoder enumerated type 
 
-  aState = digitalRead(CLK); // Reads the "current" state of the CLK
-  // If the previous and the current state of the CLK are different, that means a Pulse has occured
-  if (aState != aLastState){     
-    // If the DT state is different to the outputA state, that means the encoder is rotating clockwise
-    if (digitalRead(DT) != aState) { 
-      counter ++;
-    } else {
-      counter --;
+  static int pos = 0;
+  rotary_encoder.tick();
+  newPos = rotary_encoder.getPosition();
+
+  if (pos != newPos) {
+    settings_time = millis(); // reset settings menu timer
+    settings_time_old = millis();
+
+    if(pos < newPos){
+      rotation = clockwise;
     }
-    Serial.print("Position: ");
-    Serial.println(counter);
-  } 
-  aLastState = aState; // Updates the previous state of the outputA with the current state
+    else if(pos > newPos){
+      rotation = counter_clockwise;
+    }
+    pos = newPos;
+  }
+
+  else{
+    rotation = nothing;
+  }
+
+  return rotation;
+}
+
+int encoder_count(int max){ // uses encoder to adjust between 0 and max number provided
+  int newPos;
+  static int counter = 5;
+  static int pos = 0;
+  rotary_encoder.tick();
+  newPos = rotary_encoder.getPosition();
+
+  if (pos != newPos) {
+    settings_time = millis(); // reset settings menu timer
+    settings_time_old = millis();
+
+    Serial.print("counter; ");
+    Serial.print(counter);
+
+    if(pos > newPos){
+      counter--;
+      if(counter < 1){counter = max;}
+    }
+    else if(pos < newPos){
+      counter++;
+      if(counter > max){counter = 1;}
+    }
+    pos = newPos;
+  }
+
+  return counter;
 
 }
